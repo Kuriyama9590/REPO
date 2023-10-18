8c03a1109b08de56b0b8e51c17faeda14d521b1c
Ise_217 <120696737+Kuriyama9590@users.noreply.github.com>
2023-10-18 10:49:54 +0800

    Add files via upload

---
+++ 多窗口银行排队(WA).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int N = s.length();
+    int maxLength = 0;
+
+    unordered_set<char> charSet;
+    int i = 0, j = 0;
+
+    while (i < N && j < N) {
+        if (charSet.find(s[j]) == charSet.end()) {
+            charSet.insert(s[j]);
+            maxLength = max(maxLength, j - i + 1);
+            j++;
+        }
+        else {
+            charSet.erase(s[i]);
+            i++;
+        }
+    }
+
+    cout << "最长不重复子串的长度为: " << maxLength << endl;
+
+    return 0;
+}
